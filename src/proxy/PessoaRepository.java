c82f5b4f5377282587199d071e4d9c332e26464b
Victor luis <victorlrpf@gmail.com>
2023-11-22 19:01:56 -0300

    Criando o conteudo da classe de pessoa repository, para simular um bd

--- src/proxy/PessoaRepository.java
+++ src/proxy/PessoaRepository.java
@@ -1,4 +1,26 @@
 package proxy;
 
+import builder.Pessoa;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PessoaRepository {
+// Basicamente aqui vai simular o comportamento de um BD
+    private Map<Long, Pessoa> banco = new HashMap<>();
+    private static Long countId = 1L;
+
+    public void save(Pessoa pessoa){
+        banco.put(countId++, pessoa);
+    }
+
+    public Pessoa findById(Long id){
+        try {
+            Thread.sleep(3000); // Essa thread vai dormir por esse tempo
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return banco.get(id);
+    }
+
 }
